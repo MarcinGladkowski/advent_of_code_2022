69f31107b4f0d9fb9ea16f46adfc9f219d73985d
Marcin Gladkowski <marcin.gladkowski@xcaliber.com>
2022-12-02 15:28:06 +0100

    day_1: done

---
+++ day_1/main.py
@@ -0,0 +1,35 @@
+print("Day 1 results:")
+
+
+def read_data(file: str):
+    with open(file) as f:
+        return f.readlines()
+
+
+def sums(data: [str]):
+    sanitized = [x.replace("\n", "") for x in data]
+
+    sums = [0]
+    for el in sanitized:
+        if el != '':
+            sums[len(sums) - 1] += int(el)
+            continue
+
+        sums.append(0)
+
+    return sums
+
+
+def sum_top_of_three(sums: [int]):
+    sums.sort(reverse=True)
+    return sum(sums[:3])
+
+
+data = read_data('test_input.txt')
+assert 24000 == max(sums(data))
+assert 45000 == sum_top_of_three(sums(data))
+
+aof_data = read_data('input.txt')
+assert 70509 == max(sums(aof_data))
+print(sum_top_of_three(sums(aof_data)))
+
