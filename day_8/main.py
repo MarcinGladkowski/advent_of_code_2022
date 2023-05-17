22c46272478afdab4f16642a6c636ada698daaba
MarcinGladkowski <marcingladkowski@gmail.com>
2023-05-17 22:07:36 +0200

    day8: filtering trees on edge

--- day_8/main.py
+++ day_8/main.py
@@ -9,69 +9,38 @@ forrest = [
 ]
 
 
-class Coordinates:
+def is_on_edge(x: int, y: int, area: list) -> bool:
+    if x == 0 or y == 0:
+        return True
 
-    def __init__(self, x: int, y: int) -> None:
+    if x == len(area[0]) - 1 or y == len(area[0]) - 1:
+        return True
+
+
+class Coordinates:
+    """
+    on_edge: always visible
+    """
+
+    def __init__(self, x: int, y: int, on_edge: bool) -> None:
         self.x = x
         self.y = y
+        self.on_edge = on_edge
 
 
-grid = {
-    'top_left': Coordinates(1, 1),
-    'top_right': Coordinates(len(forrest) - 1, 1),
-    'bottom_left': Coordinates(1, len(forrest) - 1),
-    'bottom_right': Coordinates(len(forrest) - 1, len(forrest) - 1),
-}
+class Tree:
+    def __init__(self, height: int, coordinates: Coordinates):
+        self.height = height
+        self.coordinates = coordinates
 
 
-class Axle:
+transformed_forrest = []
 
-    def __init__(self, a, b) -> None:
-        self.a = a
-        self.b = b
-
-    def on_axle(self, coord):
-        return True if self.a < coord < self.b else False
-
-
-xaxle = Axle(0, 4)
-yaxle = Axle(0, 4)
-"""
-first step: display elements inside grid
-"""
-coords_to_check = []
-for i, row in enumerate(forrest):
-    for j, el in enumerate(row):
-        coords = Coordinates(j, i)
-        if xaxle.on_axle(coords.x) and yaxle.on_axle(coords.y):
-            coords_to_check.append(coords)
-            continue
-
-assert 9 == len(coords_to_check)
+for y, row in enumerate(forrest):
+    for x, el in enumerate(row):
+        transformed_forrest.append(
+            Tree(el, Coordinates(x, y, is_on_edge(x, y, forrest)))
+        )
 
 
-def filter_higher_trees(tree_index: int, row: list):
-    return len(list(filter(lambda x: x >= row[tree_index], row))) > 0
-
-
-assert filter_higher_trees(2, [2, 5, 5, 1, 2])
-
-
-def get_column_values(column_index: int, forrest: list):
-    return [x[column_index] for x in forrest]
-
-
-def check_visibility(coords_to_review: list, forrest: list):
-    visible_coords = []
-    for coord in coords_to_review:
-
-        if filter_higher_trees(coord.x, forrest[coord.y]) \
-                or filter_higher_trees(coord.y, get_column_values(coord.x, forrest)):
-            visible_coords.append(coord)
-
-    return visible_coords
-
-
-result = check_visibility(coords_to_check, forrest)
-
-print(len(result))
+assert 16 == len(list(filter(lambda tree: tree.coordinates.on_edge, transformed_forrest)))
