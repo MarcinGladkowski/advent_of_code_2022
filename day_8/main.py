dd69df031e4781d4a4a617f54e5e343db1edc82d
MarcinGladkowski <marcingladkowski@gmail.com>
2023-05-18 08:01:44 +0200

    day8: testing solution

--- day_8/main.py
+++ day_8/main.py
@@ -1,4 +1,13 @@
-from typing import Type
+from helper.main import read_data, sanitize, FILE
+
+print("Day 8")
+
+data = sanitize(read_data(FILE))
+
+prepared_data = []
+for row in data:
+    prepared_data.append(list(map(lambda x: int(x), list(row))))
+
 
 forrest = [
     [3, 0, 3, 7, 3],
@@ -34,39 +43,64 @@ class Tree:
         self.coordinates = coordinates
 
 
-transformed_forrest = []
+def transform_forrest(data: list):
+    transformed_forrest = []
+    for y, row in enumerate(data):
+        for x, el in enumerate(row):
+            transformed_forrest.append(
+                Tree(el, Coordinates(x, y, is_on_edge(x, y, forrest)))
+            )
 
-for y, row in enumerate(forrest):
-    for x, el in enumerate(row):
-        transformed_forrest.append(
-            Tree(el, Coordinates(x, y, is_on_edge(x, y, forrest)))
-        )
+    return transformed_forrest
+
+
+transformed_forrest = transform_forrest(forrest);
 
 assert 16 == len(list(filter(lambda tree: tree.coordinates.on_edge, transformed_forrest)))
 
 
-def trees_on_axle_x(tree: Tree, area: list):
+def tree_visibility_on_axle_x(tree: Tree, area: list):
     x_trees = list(filter(lambda el: el.coordinates.y == tree.coordinates.y, area))
 
-    left_side_trees_higher = filter(lambda el: el.height < tree.height, x_trees[:][0:tree.coordinates.x])
-    right_side_trees_higher = filter(lambda el: el.height < tree.height, x_trees[:][tree.coordinates.x + 1:])
+    left_side_trees_higher = list(filter(lambda el: el.height >= tree.height, x_trees[:][0:tree.coordinates.x]))
+    right_side_trees_higher = list(filter(lambda el: el.height >= tree.height, x_trees[:][tree.coordinates.x + 1:]))
 
-    return True if len(list(left_side_trees_higher)) > 0 and len(list(right_side_trees_higher)) > 0 else False
-
-
-assert True == trees_on_axle_x(Tree(5, Coordinates(1, 1, False)), transformed_forrest)
-assert False == trees_on_axle_x(Tree(5, Coordinates(2, 2, False)), transformed_forrest)
+    return False if len(left_side_trees_higher) > 0 and len(right_side_trees_higher) > 0 else True
 
 
-def visible_trees(forrest: list):
+assert True == tree_visibility_on_axle_x(Tree(5, Coordinates(1, 1, False)), transformed_forrest)
+assert False == tree_visibility_on_axle_x(Tree(3, Coordinates(2, 2, False)), transformed_forrest)
+
+
+def tree_visibility_on_axle_y(tree: Tree, area: list):
+    y_trees = list(filter(lambda el: el.coordinates.x == tree.coordinates.x, area))
+
+    up_side_trees_higher = list(filter(lambda el: el.height >= tree.height, y_trees[:][0:tree.coordinates.y]))
+    down_side_trees_higher = list(filter(lambda el: el.height >= tree.height, y_trees[:][tree.coordinates.y + 1:]))
+
+    return False if len(up_side_trees_higher) > 0 and len(down_side_trees_higher) > 0 else True
+
+
+assert True == tree_visibility_on_axle_y(Tree(5, Coordinates(1, 1, False)), transformed_forrest)
+assert False == tree_visibility_on_axle_y(Tree(5, Coordinates(2, 2, False)), transformed_forrest)
+
+
+def visible_trees(test_forrest: list):
     count = 0
-    for tree in forrest:
+    for tree in test_forrest:
         if tree.coordinates.on_edge:
+            count = count + 1
+            continue
+
+        if tree_visibility_on_axle_x(tree, test_forrest) or tree_visibility_on_axle_y(tree, test_forrest):
             count = count + 1
 
     return count
 
 
+assert 21 == visible_trees(transformed_forrest)
+
 print(
-    visible_trees(transformed_forrest)
+    visible_trees(transform_forrest(prepared_data))
 )
+
