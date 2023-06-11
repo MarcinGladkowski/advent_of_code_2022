369279370d1662dcdd9491e2d709cd63b531fffd
MarcinGladkowski <marcingladkowski@gmail.com>
2023-06-11 13:21:45 +0200

    day_8: calculated highest scenic on test forrest

--- day_8/tree_house_scenic.py
+++ day_8/tree_house_scenic.py
@@ -1,3 +1,5 @@
+import functools
+import math
 from typing import Callable
 
 from day_8.forrest import transform_forrest, Tree, Coordinates
@@ -37,16 +39,17 @@ def scenic_for_tree_axle_y(tree: Tree, area: list, count_scenic: Callable):
 
 def count_visible_scenic_trees(trees: list, tree: Tree):
     counter = 0
+
+    if len(trees) == 0:
+        return 1
+
     for comparable_tree_height in trees:
 
         if tree.height > comparable_tree_height:
             counter += 1
             continue
 
-        if tree.height == comparable_tree_height:
-            counter += 1
-            break
-
+        counter += 1
         break
 
     return counter
@@ -56,3 +59,20 @@ tree_to_calculate_scenic = Tree(5, Coordinates(2, 1, False))
 
 assert scenic_for_tree_axle_x(tree_to_calculate_scenic, test_forrest, count_visible_scenic_trees) == (1, 2)
 assert scenic_for_tree_axle_y(tree_to_calculate_scenic, test_forrest, count_visible_scenic_trees) == (1, 2)
+
+
+def highest_tree_scenic(scenic_forrest: list):
+    highest_scenic = 0
+    for tree in scenic_forrest:
+        scenic_count_for_tree = scenic_for_tree_axle_x(tree, scenic_forrest, count_visible_scenic_trees) \
+                                + scenic_for_tree_axle_y(tree, scenic_forrest, count_visible_scenic_trees)
+
+        calculated = functools.reduce(lambda a, b: a * b, scenic_count_for_tree)
+
+        if calculated > highest_scenic:
+            highest_scenic = calculated
+
+    return highest_scenic
+
+
+assert 16 == highest_tree_scenic(test_forrest)
