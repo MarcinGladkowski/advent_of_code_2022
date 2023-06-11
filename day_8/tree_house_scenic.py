e080bab3ed95f5a2f00fb23812135511a46847a0
MarcinGladkowski <marcingladkowski@gmail.com>
2023-06-11 12:55:07 +0200

    day_8: calculate scenic on axle y

--- day_8/tree_house_scenic.py
+++ day_8/tree_house_scenic.py
@@ -10,19 +10,29 @@ forrest = [
 
 test_forrest = transform_forrest(forrest)
 
-tree_to_calculate_scenic = Tree(5, Coordinates(2, 1, False))
-
 
 def scenic_for_tree_axle_x(tree: Tree, area: list):
+
     x_trees = list(filter(lambda el: el.coordinates.y == tree.coordinates.y, area))
 
     left_trees = list(map(lambda row_tree: row_tree.height, x_trees[:tree.coordinates.x]))
-
     left_trees.sort(reverse=True)
 
-    right_trees = list(map(lambda row_tree: row_tree.height, x_trees[tree.coordinates.x+1:]))
+    right_trees = list(map(lambda row_tree: row_tree.height, x_trees[tree.coordinates.x + 1:]))
 
     return count_visible_scenic_trees(left_trees, tree), count_visible_scenic_trees(right_trees, tree)
+
+
+def scenic_for_tree_axle_y(tree: Tree, area: list):
+
+    y_trees = list(filter(lambda el: el.coordinates.x == tree.coordinates.x, area))
+
+    up_trees = list(map(lambda row_tree: row_tree.height, y_trees[:tree.coordinates.y]))
+    up_trees.sort(reverse=True)
+
+    down_trees = list(map(lambda row_tree: row_tree.height, y_trees[tree.coordinates.y + 1:]))
+
+    return count_visible_scenic_trees(up_trees, tree), count_visible_scenic_trees(down_trees, tree)
 
 
 def count_visible_scenic_trees(trees: list, tree: Tree):
@@ -42,4 +52,10 @@ def count_visible_scenic_trees(trees: list, tree: Tree):
     return counter
 
 
+
+tree_to_calculate_scenic = Tree(5, Coordinates(2, 1, False))
+
+scenic_for_tree_axle_y(tree_to_calculate_scenic, test_forrest)
+
 assert scenic_for_tree_axle_x(tree_to_calculate_scenic, test_forrest) == (1, 2)
+assert scenic_for_tree_axle_y(tree_to_calculate_scenic, test_forrest) == (1, 2)
